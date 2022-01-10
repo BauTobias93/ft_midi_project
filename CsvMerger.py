f532fb6c7fcf180228fa0a337bd4c7c7487c70c1
ppreiser <preiser.philipp@gmail.com>
2022-01-10 10:34:33 +0100

    - add real_time column to merged csv

--- CsvMerger.py
+++ CsvMerger.py
@@ -2,6 +2,12 @@ import os
 import glob
 import pandas as pd
 
+
+def truncate(num, n):
+    integer = int(num * (10**n))/(10**n)
+    return float(integer)
+
+
 os.chdir('csv_files')
 all_filenames = [i for i in glob.glob('*.{}'.format('csv'))]
 
@@ -15,14 +21,17 @@ for f in all_filenames:
     case_id_col = []
     octave_col = []
     note_col = []
+    real_time_col = []
     for index, row in df.iterrows():
         case_id_col.append(current_case_id)
         octave_col.append(row["note_name"][-1])
         note_col.append(row["note_name"][:-1].replace("-", ""))
+        real_time_col.append(truncate((row["start_time"] / (row["tempo"] / 60)), 2))
 
     df.insert(0, 'case_id', case_id_col)
     df.insert(2, 'octave', octave_col)
     df.insert(2, 'note', note_col)
+    df.insert(6, 'real_time', real_time_col)
     csv_files.append(df)
 
     current_case_id += 1
