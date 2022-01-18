558ee0d15a07b44f56b438df4d572af320d4bfae
ppreiser <preiser.philipp@gmail.com>
2022-01-18 09:47:54 +0100

    add genre and song name col limit to 20 songs per genre shuffle songlist

--- CsvMerger.py
+++ CsvMerger.py
@@ -1,6 +1,8 @@
 import os
 import glob
 import pandas as pd
+import re
+import random
 
 
 def truncate(num, n):
@@ -13,29 +15,45 @@ EXT = "*.csv"
 all_csv_files = [file
                  for path, subdir, files in os.walk(PATH)
                  for file in glob.glob(os.path.join(path, EXT))]
-
+random.shuffle(all_csv_files)
 csv_files = []
 
 current_case_id = 0
-
+pattern = re.compile(r"(.*)\\(.*)\\(.*).csv")
+songs_pre_genre = {}
 for f in all_csv_files:
     print(f)
     df = pd.read_csv(f)
+    genre = pattern.match(f).group(2)
+    song_name = pattern.match(f).group(3)
+    if genre in songs_pre_genre:
+        if songs_pre_genre[genre] >= 20:
+            continue
+        else:
+            songs_pre_genre[genre] = songs_pre_genre[genre] + 1
+    else:
+        songs_pre_genre[genre] = 1
 
     case_id_col = []
     octave_col = []
     note_col = []
     real_time_col = []
+    genre_col = []
+    song_name_col = []
     for index, row in df.iterrows():
         case_id_col.append(current_case_id)
         octave_col.append(row["note_name"][-1])
         note_col.append(row["note_name"][:-1].replace("-", ""))
         real_time_col.append(truncate((row["start_time"] / (row["tempo"] / 60)), 2))
+        genre_col.append(genre)
+        song_name_col.append(song_name)
 
     df.insert(0, 'case_id', case_id_col)
     df.insert(2, 'octave', octave_col)
-    df.insert(2, 'note', note_col)
+    df.insert(2, 'actual_note', note_col)
     df.insert(6, 'real_time', real_time_col)
+    df.insert(2, 'genre', genre_col)
+    df.insert(3, 'song_name', song_name)
     csv_files.append(df)
 
     current_case_id += 1
